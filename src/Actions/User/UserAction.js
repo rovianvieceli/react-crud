a7a899a14396efaa8413238d4f878e714c1144d2
Rovian Vieceli <rovian.vieceli@gmail.com>
2019-10-22 01:29:26 +0000

    Enviando dados para cadastro

--- src/Actions/User/UserAction.js
+++ src/Actions/User/UserAction.js
@@ -1,3 +1,13 @@
 export const submitUserAction = (data) => {
-    console.log(data)
+    const base_url = "http://localhost:8080/user";
+
+    fetch(base_url, {
+        method: "post",
+        headers: {
+            'Content-type': 'application/json'
+        },
+        body: JSON.stringify(data)
+            .then(response => response.JSON())
+            .then(json => { console.log(json) })
+    })
 }
